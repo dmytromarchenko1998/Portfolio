da2badc4b9de9b4d37c4d4fc1ce8143bb782bbee
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-07-31 22:14:43 -0700

    updated contact links and have basic setup for projects

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,12 +6,28 @@ class contact extends React.Component  {
     super(props)
     this.state = {
       selected: 'none',
+      type:'none',
     }
-    this.updateSelected = this.updateSelected.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.unToggle = this.unToggle.bind(this);
   }
 
-  updateSelected(type) {
-    this.setState({selected:type})
+  // updateSelected(selected) {
+  //   this.setState({selected:selected})
+  // }
+
+  toggle(project, type) {
+    if ((this.state.selected === project) && (this.state.type === 'click')) {
+      this.setState({selected:'none', type: type});
+    } else {
+      this.setState({selected:project, type: type})
+    }
+  }
+
+  unToggle(type) {
+    if (this.state.type === 'hover') {
+      this.setState({selected:'none', type: type})
+    }
   }
 
   render() {
@@ -20,18 +36,18 @@ class contact extends React.Component  {
         <div className="contact-container">
           <h1 className="section-header">Contact Me</h1>
           <div className="me"></div>
-          <ContactModalContainer updateSelected={this.updateSelected} selected={this.state.selected}/>
+          <ContactModalContainer toggle={this.toggle} selected={this.state.selected}/>
           <div className="contact-info">
-            <a href="https://www.linkedin.com/in/dmytro-marchenko/" target="_blank" onMouseLeave={() => {this.updateSelected('none')}} onMouseEnter={() => {this.updateSelected('linkedin')}} className="linkedin link">
+            <a link="https://www.linkedin.com/in/dmytro-marchenko/" target="_blank" onClick={() => {this.toggle('linkedin', 'click')}}onMouseLeave={() => {this.unToggle('hover')}} onMouseEnter={() => {this.toggle('linkedin')}} className="linkedin link">
               <div className="linkedin hover"></div>
             </a>
-            <a href="https://github.com/dmytromarchenko1998" target="_blank" onMouseLeave={() => {this.updateSelected('none')}} onMouseEnter={() => {this.updateSelected('github')}} className="github link">
+            <a link="https://github.com/dmytromarchenko1998" target="_blank" onClick={() => {this.toggle('github', 'click')}}onMouseLeave={() => {this.unToggle('hover')}} onMouseEnter={() => {this.toggle('github')}} className="github link">
               <div className="github hover"></div>
             </a>
-            <a href="mailto:dmytromarchenko1998@gmail.com" onMouseLeave={() => {this.updateSelected('none')}} onMouseEnter={() => {this.updateSelected('mail')}} className="mail link">
+            <a link="mailto:dmytromarchenko1998@gmail.com" onClick={() => {this.toggle('mail', 'click')}}onMouseLeave={() => {this.unToggle('hover')}} onMouseEnter={() => {this.toggle('mail', 'hover')}} className="mail link">
               <div className="mail hover"></div>
             </a>
-            <a href="tel:925-457-1925" onMouseLeave={() => {this.updateSelected('none')}} onMouseEnter={() => {this.updateSelected('phone')}} className="phone link">
+            <a link="tel:925-457-1925" onClick={() => {this.toggle('phone', 'click')}}onMouseLeave={() => {this.unToggle('hover')}} onMouseEnter={() => {this.toggle('phone')}} className="phone link">
               <div className="phone  hover"></div>
             </a>
           </div>
---
+++ src/components/project.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const project = ({ projectData, toggle, unToggle, selected, type }) => {
+  let projectName = projectData.id;
+  if (projectData.id === 'gottago') {
+    projectName = 'Gotta Go';
+  }
+  if (projectData.id === selected) {
+    if (type === 'hover') {
+      return (
+        <div id={projectData.id} onMouseLeave={() => {unToggle(projectData.id, 'hover')}} onMouseEnter={() => {toggle(projectData.id, 'hover')}} onClick={() => {toggle(projectData.id, 'click')}} className="project viewmore">
+          <div>
+            <div><h4>View More</h4></div>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div id={projectData.id} onMouseLeave={() => {unToggle(projectData.id, 'hover')}} onMouseEnter={() => {toggle(projectData.id, 'hover')}} onClick={() => {toggle(projectData.id, 'click')}} className="project">
+          <div>
+            <div><h4>selected</h4></div>
+          </div>
+        </div>
+      )
+    }
+  } else {
+    return (
+      <div id={projectData.id} onMouseEnter={() => {toggle(projectData.id, 'hover')}} onClick={() => {toggle(projectData.id, 'click')}} className="project cover">
+      <div className="blur layer"><div className="gradient layer"></div></div>
+        <h4>{projectName}</h4>
+      </div>
+    )
+  }
+}
+
+module.exports = project;
