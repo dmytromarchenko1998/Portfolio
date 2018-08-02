89fe4e2d99c73194b7c66ddc0bbbb7a0739166da
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-08-01 22:02:15 -0700

    mobile

--- src/components/expandednav.js
+++ src/components/expandednav.js
@@ -4,11 +4,11 @@ const ExpandedNav = ({ scrollTo, toggleNav, expanded }) => {
   if (expanded) { 
     return (
       <div className="expanded-nav">
-        <li onClick={() => {scrollTo('#cover')}} className="first nav item"><a>Home</a></li>
-        <li onClick={() => {scrollTo('#projects')}} className="nav item"><a>Projects</a></li>
-        <li onClick={() => {scrollTo('#technologies')}} className="nav item"><a>Technologies</a></li>
-        <li onClick={() => {scrollTo('#about')}} className="nav item"><a>About</a></li>
-        <li onClick={() => {scrollTo('#contact')}} className="nav item"><a>Contact Me</a></li>
+        <li onClick={() => {toggleNav(); scrollTo('#cover');}} className="first nav item"><a>Home</a></li>
+        <li onClick={() => {toggleNav(); scrollTo('#projects');}} className="nav item"><a>Projects</a></li>
+        <li onClick={() => {toggleNav(); scrollTo('#technologies');}} className="nav item"><a>Technologies</a></li>
+        <li onClick={() => {toggleNav(); scrollTo('#about');}} className="nav item"><a>About</a></li>
+        <li onClick={() => {toggleNav(); scrollTo('#contact');}} className="nav item"><a>Contact Me</a></li>
       </div>
     )
   } else {
