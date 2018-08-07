7bf821e226d67f4cffcd8996206496cef1d1e4d3
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-08-07 12:02:34 -0700

    stress testing

--- server/app.js
+++ server/app.js
@@ -6,4 +6,6 @@ const app = express();
 
 app.use('/', express.static(path.join(__dirname, '/../dist')));
 
+app.get('/loaderio-bc62e2a4b191bdaeedb6af8dd45f58be', (req, res) => {res.send('loaderio-bc62e2a4b191bdaeedb6af8dd45f58be')})
+
 module.exports = app;
