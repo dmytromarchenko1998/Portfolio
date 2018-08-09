7c35abeee78901d973aa958ac77d78fbc541521d
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-08-09 15:11:36 -0700

    removing alert modal

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ContactModalContainer from './contact-modal-container';
 import ContactLinks from './contactlinks';
-import AlertModal from './alertModal';
 
 class contact extends React.Component  {
   constructor(props) {
@@ -15,8 +14,6 @@ class contact extends React.Component  {
     this.toggle = this.toggle.bind(this);
     this.unToggle = this.unToggle.bind(this);
     this.closeModal = this.closeModal.bind(this);
-    this.openAlertModal = this.openAlertModal.bind(this);
-    this.closeAlertModal = this.closeAlertModal.bind(this);
   }
 
   componentDidMount() {
@@ -52,27 +49,15 @@ class contact extends React.Component  {
     }
   }
 
-  openAlertModal(item) {
-    // const data = item.target.parentNode.previousSibling.getAttribute('value');
-    this.setState({alertModal:{type:'copy', data:`${'test'} has been copied`}});
-    setTimeout(this.closeAlertModal, 1500);
-  }
-
-  closeAlertModal() {
-    console.log(this.state);
-    this.setState({alertModal:{type:'none', data:undefined}});
-  }
-
   render() {
     return (
       <section id="contact">
         <div className="contact-container">
           <h1 className="section-header">Contact Me</h1>
           <span className="me"></span>
-          <ContactModalContainer openAlertModal={this.openAlertModal} unToggle={this.unToggle} toggle={this.toggle} selected={this.state.selected}/>
-          <ContactLinks  unToggle={this.unToggle} toggle={this.toggle} selected={this.state.selected}/>
+          <ContactModalContainer unToggle={this.unToggle} toggle={this.toggle} selected={this.state.selected}/>
+          <ContactLinks unToggle={this.unToggle} toggle={this.toggle} selected={this.state.selected}/>
         </div>
-        <AlertModal type={this.state.alertModal.type} data={this.state.alertModal.data}/>
       </section>
     );
   };
