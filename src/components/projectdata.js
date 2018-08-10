c4cdec5e070facb4396b1ff3cdc22e53dfac9896
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-08-10 14:50:42 -0700

    added data

--- src/components/projectdata.js
+++ src/components/projectdata.js
@@ -3,9 +3,7 @@ import React from 'react';
 const ProjectData = ({ pageData }) => {
   return (
     <div className="projectData">
-      {pageData.map((item) => {
-        return (<p>{item}</p>)
-      })}
+      <p>{pageData}</p>
     </div>
   )
 }
