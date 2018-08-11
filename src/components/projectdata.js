5f2e91a463ade6657afce92307fbacd232b39799
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-08-10 19:07:45 -0700

    finished styling projectData and images

--- src/components/projectdata.js
+++ src/components/projectdata.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
 const ProjectData = ({ pageData }) => {
+  const checkImage = (image) => {
+    if (image !== undefined) {
+      document.documentElement.style.setProperty('--project-image', `url(${image})`);
+      return <div className="project-image"></div>
+    } else {
+      return null;
+    }
+  }
+
+  const checkData = (data) => {
+    if (data !== undefined) {
+      return <p>{data}</p>
+    } else {
+      return null;
+    }
+  }
   return (
     <div className="projectData">
-      <p>{pageData}</p>
+      {checkImage(pageData.image)}
+      {checkData(pageData.data)}
     </div>
   )
 }
